0f2c8ff6987fc43a3d4734b2c7633ed957283604
Breno Macêdo <55261375+brenosekiro@users.noreply.github.com>
2020-01-24 16:20:38 -0300

    Update api.js

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 require('dotenv').config()
 
 const api = axios.create({
-    baseURL: process.env.BASEURL
+    baseURL: process.env.REACT_APP_BASEURL
 })
 
 export default api
